18e3f26a2002003c39d149881974537dafd3e7da
Jack Fagan <jhfagan579@gmail.com>
2020-02-28 11:00:25 -0600

    added .gitignore, set up some basic framework

---
+++ ISZSongSearch/SongRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ISZSongSearch
+{
+    public class SongRepository //: ISongRepository
+    {
+        //public IEnumerable<Song> GetAllSongs();
+        //public Song GetSong(int id);
+    }
+}
